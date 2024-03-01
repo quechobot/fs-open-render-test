26bf3cf3bd3ead37ad39cbd5ce74ee12d898c140
cristopher Quecholac <alum.2c@gmail.com>
2024-03-01 10:26:45 -0600

    added dist from build app notes

--- index.js
+++ index.js
@@ -33,11 +33,6 @@ let notes = [
 ]
 const app = express()
 const cors = require('cors')
-app.use(cors())
-app.use(express.json())
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
@@ -54,6 +49,9 @@ const requestLogger = (request, response, next) => {
     console.log('---')
     next()
 }
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
 app.use(requestLogger)
 app.post('/api/notes', (request, response) => {
     const body = request.body
