9dbda9e030d276bf132c97bda7cb85ca4945b5ee
cristopher Quecholac <alum.2c@gmail.com>
2024-03-04 17:02:17 -0600

    enable put and delete with mongoose

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const errorHandler = (error, request, response, next) => {
     }
     next(error)
 }
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
@@ -45,26 +52,26 @@ app.post('/api/notes', (request, response) => {
     })
 })
 app.put('/api/notes/:id', (request, response)=>{
-    response.status(405).send({error: 'Method Not Allowed'})
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        }).catch(error => next(error))
 })
 app.delete('/api/notes/:id', (request, response) => {
-    /*const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()*/
-    response.status(405).send({error: 'Method Not Allowed'})
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        }).catch(error => next(error))
 })
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
 app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
