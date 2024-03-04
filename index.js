09dd1e766ce651905ad0082576fb1c47006f7ccc
cristopher Quecholac <alum.2c@gmail.com>
2024-03-04 16:54:39 -0600

    manejo de errores en middleware fixed

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
@@ -22,7 +15,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(requestLogger)
-app.use(errorHandler)
 
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
@@ -66,6 +58,14 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
