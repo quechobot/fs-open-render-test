3436217c57bbc4c4700bfe2e3706c466acefea63
cristopher Quecholac <alum.2c@gmail.com>
2024-03-05 11:54:03 -0600

    validations with mongoose

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
@@ -38,7 +40,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     }).catch(error => next(error))
 
 })
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
     if (body.content === undefined) {
         return response.status(400).json({error: 'content missing'})
@@ -49,15 +51,19 @@ app.post('/api/notes', (request, response) => {
     })
     note.save().then(savedNote => {
         response.json(savedNote)
-    })
+    }).catch(error => next(error))
+
 })
 app.put('/api/notes/:id', (request, response)=>{
+    const { content, important } = request.body
     const body = request.body
     const note = {
         content: body.content,
         important: body.important,
     }
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: 'query' })
         .then(updatedNote => {
             response.json(updatedNote)
         }).catch(error => next(error))
