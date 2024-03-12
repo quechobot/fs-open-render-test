4e4cf86cb83a48af92c63e75436335dd2fd72283
cristopher Quecholac <alum.2c@gmail.com>
2024-03-12 13:29:05 -0600

    prueabas unitarias y tests

--- models/note_old.js
+++ models/note_old.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose')
-
+mongoose.set('strictQuery', false)
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
-        required: true,
-        minlength: 5
+        minLength: 5,
+        required: true
     },
     important: Boolean,
 })
-
 noteSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
@@ -16,5 +24,4 @@ noteSchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
-
 module.exports = mongoose.model('Note', noteSchema)
