0866402807b4acc76ba0b80919fc03e59ec652a9
Cristopher <alum.2c@gmail.com>
2024-03-20 13:55:53 -0600

    part4 b) creando notas

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,7 @@
+const User = require('../models/user')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+
 
 notesRouter.get('/', async (request, response) => {
     const notes = await Note.find({})
@@ -17,11 +19,16 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
     const body = request.body
+    const user = await User.findById(body.userId)
     const note = new Note({
         content: body.content,
         important: body.important || false,
+        user: user.id
     })
+
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
     response.status(201).json(savedNote)
 })
 
