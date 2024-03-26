33de167a676ef17f461dc468632e2a1f027433fc
Cristopher <alum.2c@gmail.com>
2024-03-26 11:30:30 -0600

    added token expiration

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,7 +1,7 @@
 const User = require('../models/user')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
-
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
     const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -16,10 +16,23 @@ notesRouter.get('/:id', async (request, response) => {
         response.status(404).end()
     }
 })
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 notesRouter.post('/', async (request, response) => {
     const body = request.body
-    const user = await User.findById(body.userId)
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    //const user = await User.findById(body.userId)
     const note = new Note({
         content: body.content,
         important: body.important || false,
